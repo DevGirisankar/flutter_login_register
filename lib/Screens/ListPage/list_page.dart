3414b45bad08dbb1076f5e15a456f4528ad6a99f
Giri <girisankar.g@iinerds.com>
2021-11-03 14:53:38 +0530

    changes

--- lib/Screens/ListPage/list_page.dart
+++ lib/Screens/ListPage/list_page.dart
@@ -1,17 +1,17 @@
 import 'dart:convert';
-import 'dart:ui';
 import 'package:flutter_login_signup/constants.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'Componets/repositories_model.dart';
-import 'Componets/list.dart';
 
 Future<Repositories> fetchRepos([bool isfromRefresh = false]) async {
-  final response = await http
-      .get(Uri.parse('https://api.github.com/search/repositories?q=flutter'));
+  var url = isfromRefresh
+      ? 'https://api.github.com/search/repositories?q=flutter'
+      : 'https://api.github.com/search/repositories?q=swift';
+  final response = await http.get(Uri.parse(url));
   print('isfromRefresh: $isfromRefresh');
-  print('calll');
+  print(url);
   if (response.statusCode == 200) {
     print('200');
     return Repositories.fromJson(jsonDecode(response.body));
@@ -22,37 +22,17 @@ Future<Repositories> fetchRepos([bool isfromRefresh = false]) async {
   }
 }
 
-Widget _glist(AsyncSnapshot<Repositories> futureRepos) {
-  return RefreshIndicator(
-      child: ListView.builder(
-          itemBuilder: (context, int index) {
-            var data = futureRepos.data?.items[index];
-            return Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 8),
-              child: Card(
-                color: kGreyColor,
-                child: ListTile(
-                  contentPadding: const EdgeInsets.all(16),
-                  title: Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Text(data?.name ?? 'Unknown',
-                        style: const TextStyle(color: Colors.white)),
-                  ),
-                  subtitle: Text(data?.description ?? 'No description',
-                      style: const TextStyle(color: Colors.white)),
-                ),
-              ),
-            );
-          },
-          itemCount: futureRepos.data?.items.length ?? 0),
-      onRefresh: HomePage()._handleRefresh);
-}
-
 class HomePage extends State<ListPage> {
   late Future<Repositories> futureRepos;
+  @override
+  void initState() {
+    super.initState();
+    futureRepos = fetchRepos();
+  }
 
   @override
   Widget build(BuildContext context) {
+    print("building...");
     return Scaffold(
       backgroundColor: kBGColor,
       body: Center(
@@ -60,8 +40,10 @@ class HomePage extends State<ListPage> {
         future: futureRepos,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
+            print(snapshot.data?.items[0].name);
             return _glist(snapshot);
           } else if (snapshot.hasError) {
+            print('snapshot.hasError');
             return Text('${snapshot.error}');
           }
           return const CircularProgressIndicator();
@@ -78,18 +60,38 @@ class HomePage extends State<ListPage> {
     );
   }
 
-  Future<void> _handleRefresh() async {
-    print('refreeeeee');
-    // setState(() {
-    fetchRepos(true);
-    // });
-    return null;
+  Widget _glist(AsyncSnapshot<Repositories> futureRepos) {
+    return RefreshIndicator(
+        child: ListView.builder(
+            itemBuilder: (context, int index) {
+              var data = futureRepos.data?.items[index];
+              return Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 8),
+                child: Card(
+                  color: kGreyColor,
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.all(16),
+                    title: Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Text(data?.name ?? 'Unknown',
+                          style: const TextStyle(color: Colors.white)),
+                    ),
+                    subtitle: Text(data?.description ?? 'No description',
+                        style: const TextStyle(color: Colors.white)),
+                  ),
+                ),
+              );
+            },
+            itemCount: futureRepos.data?.items.length ?? 0),
+        onRefresh: _handleRefresh);
   }
 
-  @override
-  void initState() {
-    super.initState();
-    futureRepos = fetchRepos();
+  Future<void> _handleRefresh() async {
+    setState(() {
+      futureRepos = fetchRepos();
+      //  fetchRepos(true);
+    });
+    return;
   }
 }
 
